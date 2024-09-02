cce23a361cf0fba3fcf2b28287c668bfdb56781c
Kyuhyeok Seo <kyuhyeok.seo@moloco.com>
2024-09-02 22:34:43 +0900

    Time: 226 ms (48.48%), Space: 17.9 MB (34.33%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -3,102 +3,48 @@ class Solution:
         
         M = len(matrix)
         N = len(matrix[0])
-        
         out = 0
-        is_zero = 0
-        for x in range(M):
-            for y in range(N):
-                if matrix[x][y] == '1':
-                    out = 1
+        
+        heights = [0 for _ in range(N+1)]
+        for y in range(M):
+            
+            for x in range(N):
+                if matrix[y][x] == '1' :
+                    heights[x] += 1
                 else :
-                    is_zero = 1
+                    heights[x] = 0
+            
+            A = self.largest_Histogram(heights[:])
+            out = max(out, A)
         
-        
-        if out == 0 :
-            return 0
-        if is_zero == 0 :
-            return M*N
-        
-        P1 = []
-        for x in range(M):
-            P1.append(matrix[x][:])
-        
-        w = 1
-        for _ in range(M):
-            
-            print('-'*10)
-            P = []
-            for x in range(len(P1)):
-                P.append(P1[x][:])
-                
-            ###########################
-            
-            ############
-            #for z in P:
-            #    print(z)
-            ############
-            
-            h = 1
-            for _ in range(N) :
-                flag = False
-                X = len(P)
-                Y = len(P[0])
-
-                Q = []
-                for i in range(X):
-                    tmp = []
-                    for j in range(Y):
-                        if P[i][j] == '1' :
-                            flag = True
-                        if j < Y-1 :
-                            if P[i][j] == '1' and P[i][j+1] == '1' :
-                                tmp.append('1')
-                            else :
-                                tmp.append('0')
-                    Q.append(tmp[:])
-            
-                P = []
-                for x in range(len(Q)):
-                    P.append(Q[x][:])
-                
-                if flag == True :
-                    out = max(out, w*h)
-                else :
-                    break
-                h += 1
-            ##############################
-            C = len(P1)
-            D = len(P1[0])
-
-            P2 = []
-            flag2 = False
-            for ii in range(C):
-                tmp = []
-                for jj in range(D):
-                    
-                    if P1[ii][jj] == '1' :
-                            flag2 = True
-                    if ii < C-1 :
-                        if P1[ii][jj] == '1' and P1[ii+1][jj] == '1' :
-                            tmp.append('1')
-                        else :
-                            tmp.append('0')
-                            
-                if ii < C-1 :
-                    P2.append(tmp[:])
-            
-            P1 = []
-            for xx in range(len(P2)):
-                P1.append(P2[xx][:])
-                
-            if flag2 == False :
-                break
-            w += 1
-            
         return out
+        
+        
+        
+    def largest_Histogram(self, heights):
+        N = len(heights)
+        
+        MAX_A = 0
+        stack = []
+        
+        for i in range(N+1):
             
+            new_H = 0 if i == N else heights[i]
             
+            #print('-----------------')
+            #print(i, new_H)
             
+            while len(stack) > 0 and new_H < heights[stack[-1]] :
                 
-            
                 
+                I = stack.pop()
+                W = i-stack[-1]-1 if stack else i
+                H = heights[I]
+                A = W * H
+                #print(I, W, H, A)
+                MAX_A = max(MAX_A, A)
+            
+            stack.append(i)
+        
+        
+        return MAX_A
