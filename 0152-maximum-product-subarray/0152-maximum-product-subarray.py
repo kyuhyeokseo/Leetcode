1fc0790a9ed456c612b2de2716237b9d0409ab30
Kyuhyeok Seo <kyuhyeok.seo@moloco.com>
2024-09-28 14:08:29 +0900

    Time: 72 ms (55.04%), Space: 17 MB (34.12%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,34 +1,25 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
-        dp = [[-99999, 99999] for _ in range(len(nums)+1)]
+        out = -99999
         
-        dp[0][0] = 1
-        dp[0][1] = 1
+        tmp = 1
+        for i in range(len(nums)):
+            num = nums[i]
+            tmp *= num
+            out = max(out, tmp)
+            if tmp == 0 :
+                tmp = 1
         
-        out = nums[0]
-        
-        for i in range(1, len(nums)+1) :
-            
-            num = nums[i-1]
-            
-            if num > 0 :
-                dp[i][0] = max(dp[i][0], num, num * dp[i-1][0])
-                dp[i][1] = min(dp[i][1], num * dp[i-1][1])
-                out = max(out, dp[i][0])
-            
-            elif num < 0 :
-                dp[i][0] = max(dp[i][0], num, num * dp[i-1][1])
-                dp[i][1] = min(dp[i][1], num, num * dp[i-1][0])
-                out = max(out, dp[i][0])
-            
-            else :
-                dp[i][0] = dp[i][1] = 0
-                out = max(out, dp[i][0])
+        tmp = 1
+        for i in range(len(nums)-1, -1, -1):
+            num = nums[i]
+            tmp *= num
+            out = max(out, tmp)
+            if tmp == 0 :
+                tmp = 1
             
         return out
-            
-            
         
         
         
