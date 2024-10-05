9bbfd7c981d6f12ef2f95b93c798f0e2a6429eef
Kyuhyeok Seo <kyuhyeok.seo@moloco.com>
2024-10-05 10:55:53 +0900

    Time: 34 ms (88.99%), Space: 16.6 MB (16.45%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -3,28 +3,8 @@ import heapq
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         
-        def compareNum(m, n):
-            M, N = int(str(m) + str(n)), int(str(n) + str(m))
+        nums = sorted(nums, key = lambda x: str(x) * 100, reverse = True)
             
-            if M > N :
-                return 1
-            elif M == N :
-                return 0
-            else :
-                return -1
-            
-        
-        L = len(nums)
-        if L == 1 :
-            return str(nums[0])
-        
-        for level in range(1, L):
-            
-            for j in range(level, 0, -1) :
-                if compareNum(nums[j], nums[j-1]) == 1 :
-                    nums[j], nums[j-1] = nums[j-1], nums[j]
-        
-        #print(nums)
         out = ''
         for i in nums :
             out += str(i)
