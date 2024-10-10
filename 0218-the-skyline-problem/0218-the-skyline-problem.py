5f8729acd016276910aa4449d073ec141480d121
Kyuhyeok Seo <kyuhyeok.seo@moloco.com>
2024-10-10 13:39:41 +0900

    Time: 1057 ms (16.13%), Space: 23.1 MB (12.04%) - LeetHub

---
+++ 0218-the-skyline-problem/0218-the-skyline-problem.py
@@ -0,0 +1,56 @@
+import heapq
+
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        
+        ans = []
+        X_in_list, X_out_list = [], []
+        Y_list = []
+        X = 0
+        D_in, D_out = defaultdict(list), defaultdict(list)
+        x_list = []
+        
+        for item in buildings :
+            srt, end, h = item[0], item[1], item[2]
+            X_in_list.append(srt)
+            X_out_list.append(end)
+            
+            D_in[srt].append(h)
+            D_out[end].append(h)
+            x_list.append(srt)
+            x_list.append(end)
+        
+            X = max(X, item[1])
+        
+        q = [0]
+        H = 0
+        
+        x_list = list(set(x_list))
+        x_list.sort()
+        #print(x_list)
+        #print('----------')
+        
+        for i in x_list:
+            
+            if i in D_in :
+                for item in D_in[i] :
+                    heapq.heappush(q, -item)
+            
+            if i in D_out :
+                for item in D_out[i]:
+                    q.remove(-item)
+            
+            heapq.heapify(q)
+            H_new = -q[0]
+            
+            if H_new != H :
+                H = H_new
+                ans.append([i, H_new])
+            
+            #print(q)
+        
+        return ans
+        
+        
+        
+        
