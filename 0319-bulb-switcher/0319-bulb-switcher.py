7270093d6ccbba4bd5812c9a0d15ed1e9fd13d63
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2024-11-07 13:16:18 +0900

    Time: 0 ms (100.00%), Space: 16.6 MB (13.29%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -0,0 +1,17 @@
+import math
+
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        
+        if n == 0 :
+            return 0
+        elif n == 1 :
+            return 1
+        
+        return int(math.sqrt(n))
+        
+        
+        
+        
+        
+        
