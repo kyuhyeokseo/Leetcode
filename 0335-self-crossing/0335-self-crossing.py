a5e1854f60fc8e939fd1bdd5fa081dfe4c14309d
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2024-11-16 10:52:33 +0900

    Time: 43 ms (15.48%), Space: 26.6 MB (43.55%) - LeetHub

--- 0335-self-crossing/0335-self-crossing.py
+++ 0335-self-crossing/0335-self-crossing.py
@@ -8,17 +8,17 @@ class Solution:
         
         for i in range(3, L):
             if distance[i] >= distance[i-2] and distance[i-1] <= distance[i-3] :
-                print('a : ', i)
+                #print('a : ', i)
                 return True
             
             if i>=4 :
                 if distance[i-1] == distance[i-3] and distance[i-2] > distance[i-4] and distance[i-2] <= distance[i-4] + distance[i]:
-                    print('b : ', i)
+                    #print('b : ', i)
                     return True
             
             if i>=5 :
                 if distance[i] + distance[i-4] >= distance[i-2] and distance[i-1] + distance[i-5] >= distance[i-3] and distance[i-2] >= distance[i-4] and distance[i-3] >= distance[i-5] and distance[i-2] >= distance[i] and distance[i-3] >= distance[i-1]:
-                    print('c : ', i)
+                    #print('c : ', i)
                     return True
         
         return False
