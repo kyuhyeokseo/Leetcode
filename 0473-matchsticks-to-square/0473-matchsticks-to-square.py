35f974eb4eabea2908d6c46cdd37086479a9b316
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2025-02-24 10:44:39 +0900

    Time: 4120 ms (16.61%), Space: 17.7 MB (87.91%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -0,0 +1,44 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        
+        self.matchsticks = matchsticks
+        self.set = set()
+
+        L = 0
+        for s in matchsticks :
+            L += s
+        if L % 4 > 0 :
+            return False
+        
+        L = L // 4
+        return self.findSquare([L,L,L,L], 0)
+    
+
+    def findSquare(self, target_list, idx):
+        #print('Call : ', target_list, idx)
+        
+        if idx >= len(self.matchsticks):
+            for t in target_list :
+                if t != 0 :
+                    return False
+            return True
+
+        s = set()
+
+        for i in range(4):
+
+            if target_list[i] in s:
+                continue
+
+            s.add(target_list[i])
+            if target_list[i] - self.matchsticks[idx] >= 0 :
+                tmp = target_list[:]
+                tmp[i] -= self.matchsticks[idx]
+                result = self.findSquare(tmp, idx+1)
+
+                if result :
+                    return True
+
+        return False
+
+
