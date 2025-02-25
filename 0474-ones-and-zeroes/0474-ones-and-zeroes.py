de2caee8b4bc685082ce1b4461b28c15085092ee
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2025-02-25 11:53:16 +0900

    Time: 3074 ms (14.48%), Space: 69.6 MB (48.26%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -5,9 +5,9 @@ class Solution:
         dp = [[[0 for _ in range(N+1)] for _ in range(M+1)] for _ in range(L)]
 
         for l in range(L):
+            cnt0, cnt1 = strs[l].count('0'), strs[l].count('1')
             for m in range(M+1):
                 for n in range(N+1):
-                    cnt0, cnt1 = strs[l].count('0'), strs[l].count('1')
                     if l > 0 and m - cnt0 >= 0 and n-cnt1 >= 0:
                         dp[l][m][n] = max(dp[l][m][n], dp[l-1][m][n], dp[l-1][m-cnt0][n-cnt1]+1)
                     elif m - cnt0 >= 0 and n - cnt1 >= 0 :
