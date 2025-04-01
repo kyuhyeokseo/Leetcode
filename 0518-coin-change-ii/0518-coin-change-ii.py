febfd23712c240a07c94ec263abdbae41b470182
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2025-04-01 10:53:48 +0900

    Time: 115 ms (96.86%), Space: 18.1 MB (73.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -2,35 +2,12 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
         L = len(coins)
-        coins = sorted(coins, reverse = True)
-
-        dp = [[0 for _ in range(amount + 1)] for _ in range(L)]
-
-        cums = [0]
-
-        for i in range(L):
-
-            c = coins[i]
-            new_cums = set()
-
-            if i == 0:
-                for cum in cums:
-                    j = 0
-                    while cum + j*c <= amount:
-                        dp[i][cum+j*c] += 1
-                        new_cums.add(cum + j*c)
-                        j += 1
-            
-            else:
-                for cum in cums:
-                    j = 0
-                    while cum + j*c <= amount:
-                        dp[i][cum+j*c] += dp[i-1][cum]
-                        new_cums.add(cum + j*c)
-                        j += 1
-                    
-            cums = list(new_cums)[:]
         
-        return dp[-1][-1]
+        dp = [0 for _ in range(amount + 1)]
+        dp[0] = 1
 
+        for coin in coins:
+            for x in range(coin, amount+1):
+                dp[x] += dp[x-coin]
         
+        return dp[-1]
