30b6dd70fda17413cf1474ecdc9d20a9920e5ab2
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2025-07-23 13:14:10 +0900

    Time: 0 ms (100%), Space: 18.1 MB (18.43%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        
+        rec = []
+
+        for o in operations:
+            if o == '+':
+                rec.append(rec[-1]+rec[-2])
+            elif o == 'D':
+                rec.append(rec[-1] * 2)
+            elif o == 'C':
+                rec.pop()
+            else:
+                rec.append(int(o))
+            
+        return sum(rec)
