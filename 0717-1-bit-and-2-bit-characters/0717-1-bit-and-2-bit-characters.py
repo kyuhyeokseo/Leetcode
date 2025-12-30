5f051457a9cbe2f056ccb2a23d8541d11c8791fc
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2025-12-30 12:45:44 -0500

    Time: 2 ms (18.5%), Space: 17.3 MB (99.52%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -1,20 +1,10 @@
 class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
         
-        if bits[-1] != 0:
-            return False
+        bits = bits[::-1]
 
-        def isPossible(bits):
-            
-            if not bits:
-                return True
-            else:
-                if bits[0] == 0:
-                    return isPossible(bits[1:])
-                else:
-                    if len(bits) == 1:
-                        return False
-                    else:
-                        return isPossible(bits[2:])
-
-        return isPossible(bits[:-1])
+        while bits:
+            tmp = bits.pop()
+            if tmp:
+                bits.pop()
+        return not tmp
