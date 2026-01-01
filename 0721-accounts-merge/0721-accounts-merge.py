17566a91a62e04b02b516d2a039eef115e44c382
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2026-01-01 12:17:10 -0500

    Time: 128 ms (7.86%), Space: 22 MB (31.55%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,29 +1,53 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         
-        adj = {}
+        adj = defaultdict(list)
         rec = {}
 
         ret = []
 
         for i, account in enumerate(accounts):
-            flag = -1
+            account = [account[0]] + list(set(account[1:]))
             for email in account[1:]:
                 if email in rec:
-                    flag = rec[email]
-                    break
-            
-            if flag >= 0:
-                for email in account[1:]:
-                    rec[email] = flag
-                    ret[flag].append(email)
-                ret[flag] = [ret[flag][0]] + sorted(list(set(ret[flag][1:])))
-            else:
-                ret.append([account[0]])
-                for email in account[1:]:
-                    rec[email] = len(ret)-1
-                    ret[-1].append(email)
-                ret[-1] = [ret[-1][0]] + sorted(list(set(ret[-1][1:])))
+                    j = rec[email]
+                    adj[j].append(i)
+                    adj[i].append(j)
+                else:
+                    rec[email] = i
+        
+        N = len(accounts)
+        visited = [False] * N
+
+        idx = 0
+        groups = {}
+
+        for i in range(N):
+            tmp = []
+            if visited[i] == False:
+                currs = [i]
+                while currs:
+                    curr = currs.pop(0)
+                    tmp.append(curr)
+                    visited[curr] = True
+                    if curr in adj:
+                        adj_list = list(set(adj[curr]))
+                    else:
+                        adj_list = []
+                    for nei in adj_list:
+                        if visited[nei] == False:
+                            currs.append(nei)
+                groups[idx] = tmp[:]
+                idx += 1
+
+        #print(groups)
+        
+        ret = []
+        for key, value in groups.items():
+            ret.append([accounts[value[0]][0]])
+            email_list = [email for i in value for email in accounts[i][1:]]
+            ret[-1] += sorted(list(set(email_list)))
+
 
         return ret
 
