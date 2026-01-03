41c85d385d939d44770b6a170a0272c44ef222ca
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2026-01-03 15:53:27 -0500

    Time: 15 ms (24.24%), Space: 17.6 MB (69.96%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.py
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -5,15 +5,11 @@ class Solution:
         for target in range(left, right+1):
             
             flag = True
-            #print(target)
             for idx in range(len(str(target))):
                 n = int(str(target)[idx])
                 if (n == 0) or (n and target % n) :
                     flag = False
                     break
-            
-            #print(flag)
-            #print()
             if flag:
                 ret.append(target)
         
