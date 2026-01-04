c9df2a4c5cfd59eec8c8d6a1c7369cdec8b899a9
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2026-01-04 17:01:17 -0500

    Time: 807 ms (72.31%), Space: 85.1 MB (39.23%) - LeetHub

--- 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.py
+++ 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.py
@@ -7,43 +7,39 @@ class Solution:
         if N == 1:
             return 1
         
-        dp = [[0 for _ in range(N)] for _ in range(N)]
+        @lru_cache(None)
+        def dp(i,j):
+            if i > j :
+                return 0
+            elif i == j:
+                return 1
 
-        for gap in range(0, N):
-            for i in range(0, N-gap):
-                j = i+gap
+            ret = 0
 
-                #print(f'\nNow : ({i}, {j})')
-
-                if gap == 0:
-                    dp[i][j] = 1
-                else:
-                    if s[i] != s[j]:
-                        ret = 0
-                        for target in ['a', 'b', 'c', 'd']:
-                            left, right = s[i:j+1].find(target), s[i:j+1].rfind(target)
-                            #print(f'{s[i:j+1]} - TARGET {target} | INDEX: {left, right}')
-                            if left >= 0:
-                                ret += 1
-                            if left != right:
-                                ret += (dp[i+left+1][i+right-1] + 1)
-                        dp[i][j] = (ret % MAX)
+            if s[i] != s[j]:
+                ret = 0
+                for target in ['a', 'b', 'c', 'd']:
+                    left, right = s[i:j+1].find(target), s[i:j+1].rfind(target)
+                    #print(f'{s[i:j+1]} - TARGET {target} | INDEX: {left, right}')
+                    if left >= 0:
+                        ret += 1
+                    if left != right:
+                        ret += (dp(i+left+1,i+right-1) + 1)
+                return ret % MAX
+            else:
+                ret = 0
+                same = s[i]
+                for target in ['a', 'b', 'c', 'd']:
+                    if target != same:
+                        left, right = s[i:j+1].find(target), s[i:j+1].rfind(target)
+                        #print(f'TARGET {target} | INDEX: {left, right}')
+                        if left >= 0:
+                            ret += 1
+                        if left != right:
+                            ret += (dp(i+left+1,i+right-1) + 1)
                     else:
-                        ret = 0
-                        same = s[i]
-                        for target in ['a', 'b', 'c', 'd']:
-                            if target != same:
-                                left, right = s[i:j+1].find(target), s[i:j+1].rfind(target)
-                                #print(f'TARGET {target} | INDEX: {left, right}')
-                                if left >= 0:
-                                    ret += 1
-                                if left != right:
-                                    ret += (dp[i+left+1][i+right-1] + 1)
-                            else:
-                                ret += (2+dp[i+1][j-1])
-                        dp[i][j] = (ret % MAX)
-        
-        #for i in range(N):
-        #    print(dp[i])
-        
-        return dp[0][-1]
+                        ret += (2+dp(i+1,j-1))
+                return ret % MAX
+            
+        return dp(0, N-1) % MAX
+
