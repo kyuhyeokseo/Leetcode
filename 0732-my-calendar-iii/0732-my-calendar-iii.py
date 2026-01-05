500dba2c36b9b039b0fb409d41d542b8f8e4ba62
Kyuhyeok Seo <kyuhyeokseo@gmail.com>
2026-01-05 18:04:27 -0500

    Time: 630 ms (82.08%), Space: 17.7 MB (97.96%) - LeetHub

--- 0732-my-calendar-iii/0732-my-calendar-iii.py
+++ 0732-my-calendar-iii/0732-my-calendar-iii.py
@@ -1,14 +1,25 @@
 class MyCalendarThree:
+
     def __init__(self):
-        self.delta = collections.Counter()
+
+        self.delta = {}
         self.max_k = 0
 
     def book(self, startTime: int, endTime: int) -> int:
-        self.delta[startTime] += 1
-        self.delta[endTime] -= 1
+
+        if startTime in self.delta:
+            self.delta[startTime] += 1
+        else:
+            self.delta[startTime] = 1
+
+        if endTime in self.delta:
+            self.delta[endTime] -= 1
+        else:
+            self.delta[endTime] = -1
+
         curr = 0
-        for t in sorted(self.delta):
-            curr += self.delta[t]
+        for key in sorted(list(self.delta.keys())):
+            curr += self.delta[key]
             if curr > self.max_k:
                 self.max_k = curr
 
